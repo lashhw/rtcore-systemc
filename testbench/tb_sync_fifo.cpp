e2ac2d383b585f4aaf74c9783d6b2dac2fb66e60
Jacky Huang <lashhw81@gmail.com>
2023-01-21 18:57:16 +0800

    different number of producer/consumer

--- testbench/tb_sync_fifo.cpp
+++ testbench/tb_sync_fifo.cpp
@@ -3,39 +3,55 @@ using namespace sc_core;
 
 #include "../src/sync_fifo.hpp"
 
-SC_MODULE(tb_sync_fifo) {
+constexpr int FIFO_SIZE = 10;
+constexpr int NUM_CONSUMER = 1;
+constexpr int NUM_PRODUCER = 3;
+
+SC_MODULE(consumer) {
     sync_fifo_in<int> fifo_in;
-    sync_fifo_out<int> fifo_out;
-    SC_CTOR(tb_sync_fifo) {
-        SC_THREAD(producer);
-        SC_THREAD(consumer);
+    SC_CTOR(consumer) {
+        SC_THREAD(thread);
     }
-    void producer() {
-        int x = 0;
+    void thread() {
         while (true) {
-            std::cout << sc_time_stamp() << ": write request (" << x << ") sent!" << std::endl;
-            fifo_out->write(x);
-            std::cout << sc_time_stamp() << ": " << x << " written!" << std::endl;
-            x++;
-            wait(rand()%3, SC_NS);
+            wait(rand()%2, SC_NS);
+            std::cout << name() << " @ " << sc_time_stamp() << ": read request sent!" << std::endl;
+            int x;
+            fifo_in->read(x);
+            std::cout << name() << " @ " << sc_time_stamp() << ": " << x << " read!" << std::endl;
         }
     }
-    void consumer() {
-        int x;
+};
+
+SC_MODULE(producer) {
+    sync_fifo_out<int> fifo_out;
+    SC_CTOR(producer) {
+        SC_THREAD(thread);
+    }
+    void thread() {
+        int curr = 0;
         while (true) {
-            std::cout << sc_time_stamp() << ": read request sent!" << std::endl;
-            fifo_in->read(x);
-            std::cout << sc_time_stamp() << ": " << x << " read!" << std::endl;
-            wait(rand()%10, SC_NS);
+            wait(rand()%2, SC_NS);
+            std::cout << name() << " @ " << sc_time_stamp() << ": write request (" << curr << ") sent!" << std::endl;
+            fifo_out->write(curr);
+            std::cout << name() << " @ " << sc_time_stamp() << ": " << curr << " written!" << std::endl;
+            curr++;
         }
     }
 };
 
 int sc_main(int, char**) {
-    tb_sync_fifo tb_sync_fifo_i("test");
-    sync_fifo<int, 10> sync_fifo_i("sync_fifo");
-    tb_sync_fifo_i.fifo_in(sync_fifo_i);
-    tb_sync_fifo_i.fifo_out(sync_fifo_i);
+    sync_fifo<int, FIFO_SIZE, NUM_CONSUMER, NUM_PRODUCER> sync_fifo_i("sync_fifo");
+    for (int i = 0; i < NUM_CONSUMER; i++) {
+        std::string name = "consumer_" + std::to_string(i);
+        auto tmp = new consumer(name.c_str());
+        tmp->fifo_in(sync_fifo_i);
+    }
+    for (int i = 0; i < NUM_PRODUCER; i++) {
+        std::string name = "producer_" + std::to_string(i);
+        auto tmp = new producer(name.c_str());
+        tmp->fifo_out(sync_fifo_i);
+    }
 
     sc_start(100, SC_NS);
     return 0;
