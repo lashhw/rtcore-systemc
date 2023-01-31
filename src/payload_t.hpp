cc6fd9c224896b5bd56f3b77e8f47af3bcac1d0a
Jacky Huang <lashhw81@gmail.com>
2023-01-31 23:00:17 +0800

    ahh

--- src/payload_t.hpp
+++ src/payload_t.hpp
@@ -1,51 +1,24 @@
 #ifndef RTCORE_SYSTEMC_PAYLOAD_T_HPP
 #define RTCORE_SYSTEMC_PAYLOAD_T_HPP
 
-struct shader_to_trv_ctrl_t {
+struct ray_and_id_t {
     bvh::Ray<float> ray;
     int id;
 };
 
-struct trv_ctrl_to_shader_t {
-    bvh::Triangle<float>::Intersection ist_result;
-    int id;
-};
-
-struct trv_ctrl_to_bbox_ctrl_t {
-
-};
-
-struct bbox_ctrl_to_lp_t {
-
-};
-
-struct bbox_ctrl_to_hp_t {
-
-};
-
-struct lp_to_trv_ctrl_t {
-
-};
-
-struct hp_to_trv_ctrl_t {
-
-};
-
-struct trv_ctrl_to_ist_ctrl_t {
-
-};
-
-struct ist_ctrl_to_ist_t {
-
-};
-
-struct ist_to_trv_ctrl_t {
-
+struct to_trv_ctrl_t {
+    enum { SHADER, LP, HP, IST } type;
+    union {
+        ray_and_id_t ray_and_id;
+    } payload;
 };
 
 struct to_memory_t {
     enum { BBOX, NODE, TRIG_IDX, TRIG } type;
     int idx;
+};
+
+struct from_memory_t {
     union {
         float *bbox;
         bvh::Bvh<float>::IndexType *node;
--- src/rtcore/trv_ctrl.hpp
+++ src/rtcore/trv_ctrl.hpp
@@ -6,25 +6,29 @@
 
 template <int num_working_rays>
 struct trv_ctrl : public sc_module {
-    sync_fifo_in<shader_to_trv_ctrl_t> shader_in_p;
-    sync_fifo_in<int> lp_in_p;
-    sync_fifo_in<int> hp_in_p;
-    sync_fifo_in<int> ist_in_p;
-    sync_fifo_out<int> shader_out_p;
-    sync_fifo_out<int> bbox_ctrl_out_p;
-    sync_fifo_out<int> ist_ctrl_out_p;
-    sc_port<memory_if> arbiter_p;
+    sync_fifo_in<bvh::Ray<float>> shader_in_req_p;
+    sync_fifo_out<int> shader_in_resp_p;
+    sync_fifo_in<void*> lp_in_p;
+    sync_fifo_in<void*> hp_in_p;
+    sync_fifo_in<void*> ist_in_p;
+    sync_fifo_out<void*> shader_out_p;
+    sync_fifo_out<void*> bbox_ctrl_out_p;
+    sync_fifo_out<void*> ist_ctrl_out_p;
+    sync_fifo_out<to_memory_t> arbiter_out_req_p;
+    sync_fifo_in<from_memory_t> arbiter_out_resp_p;
 
-    sync_fifo<shader_to_trv_ctrl_t, num_working_rays> shader_fifo;
+    arbiter<to_trv_ctrl_t, vo
+
+    sync_fifo<to_trv_ctrl_t, num_working_rays> shader_fifo;
     sync_fifo<int, num_working_rays> free_fifo;
-    std::stack<int> stk;
+    std::stack<int> stk[num_working_rays];
 
-    sc_event c_thread_to_b_thread_valid;
+    sc_event c_thread_to_b_thread_event;
     to_memory_t c_thread_to_b_thread;
 
     SC_CTOR(trv_ctrl) {
         for (int i = 0; i < num_working_rays; i++)
-            free_fifo.direct_write(&i);
+            free_fifo.direct_write(i);
         SC_THREAD(a_thread);
         SC_THREAD(b_thread);
         SC_THREAD(c_thread);
@@ -32,60 +36,69 @@ struct trv_ctrl : public sc_module {
 
     void a_thread() {
         while (true) {
-            shader_to_trv_ctrl_t payload{};
-            shader_in_p->read(&payload);
-            free_fifo->read(&payload.id);
-            shader_fifo.write(payload);
-            wait(cycle);
+            bvh::Ray<float> ray;
+            shader_in_req_p->read(ray);
+            int id;
+            free_fifo.read(id);
+            ray_and_id_t ray_and_id{ray, id};
+            to_trv_ctrl_t to_trv_ctrl;
+            to_trv_ctrl.type = to_trv_ctrl_t::SHADER;
+            to_trv_ctrl.payload.ray_and_id = ray_and_id;
+            shader_fifo.write(to_trv_ctrl);
+            shader_in_resp_p->write(id);
         }
     }
 
     void b_thread() {
         while (true) {
-            wait(c_thread_to_b_thread_valid);
+            wait(c_thread_to_b_thread_event);
             assert(c_thread_to_b_thread.type == to_memory_t::NODE);
-            arbiter_p->request(c_thread_to_b_thread);
+            arbiter_out_req_p->write(c_thread_to_b_thread);
+            from_memory_t resp;
+            arbiter_out_resp_p->read(resp);
+            // other
+            // remember to prevent multiple request simultaneously
         }
     }
 
     void c_thread() {
-        const sc_event * in_event[3] = {
-            &shader_in_p->write_updated_event(),
-            &lp_in_p->write_updated_event(),
-            &hp_in_p->write_updated_event()
+        const sc_event *in_event[4] = {
+            &shader_fifo.data_written_event(),
+            &lp_in_p->data_written_event(),
+            &hp_in_p->data_written_event(),
+            &ist_in_p->data_written_event()
         };
-        const int * num_elements[3] = {
-            &shader_in_p->num_elements(),
+        const int *num_elements[4] = {
+            &shader_fifo.num_elements(),
             &lp_in_p->num_elements(),
-            &hp_in_p->num_elements()
+            &hp_in_p->num_elements(),
+            &ist_in_p->num_elements()
         };
         int first = 0;
         while (true) {
-            wait(*in_event[0] | *in_event[1] | *in_event[2]);
+            wait(*in_event[0] | *in_event[1] | *in_event[2] | *in_event[3]);
             wait(half_cycle);
             int choose = first;
-            for (; *num_elements[choose] == 0; choose = (choose + 1) % 3);
+            for (; *num_elements[choose] == 0; choose = (choose + 1) % 4);
             wait(half_cycle);
             switch(choose) {
                 case 0: {
-                    shader_to_trv_ctrl_t payload{};
-                    shader_in_p->read(&payload);
                     break;
                 }
                 case 1: {
-                    int request;
-                    lp_in_p->read(&request);
                     break;
                 }
                 case 2: {
-                    int request;
-                    hp_in_p->read(request);
                     break;
                 }
-                default:
+                case 3: {
+                    break;
+                }
+                default: {
                     sc_assert(false);
+                }
             }
-            first = (first + 1) % 3;
+            first = (first + 1) % 4;
         }
     }
 };
