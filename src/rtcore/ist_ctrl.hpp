59e2a8a8279070b165420ac7bea4ffa4608d89f8
Jacky Huang <lashhw81@gmail.com>
2023-02-04 11:34:54 +0800

    update ist_ctrl

--- src/rtcore/ist_ctrl.hpp
+++ src/rtcore/ist_ctrl.hpp
@@ -15,28 +15,30 @@ SC_MODULE(ist_ctrl) {
 
     void thread_1() {
         while (true) {
-            ist_ctrl_req_t req = p_trv_ctrl_req->read();
             wait(cycle);
-
+            ist_ctrl_req_t req = p_trv_ctrl_req->read();
             trv_ctrl_req_t resp;
             resp.type = trv_ctrl_req_t::IST;
             resp.ist_result.ray_and_id = req.ray_and_id;
+            resp.ist_result.intersected = false;
+
             for (int i = 0; i < req.num_trigs; i++) {
+                wait(cycle);
                 mem_req_t mem_req;
                 mem_req.type = mem_req_t::TRIG_IDX;
                 mem_req.idx = req.first_trig_idx + i;
                 p_mem_req->write(mem_req);
-                wait(cycle);
 
+                wait(cycle);
                 mem_resp_t mem_resp = p_mem_resp->read();
                 mem_req.type = mem_req_t::TRIG;
                 mem_req.idx = mem_resp.trig_idx;
                 p_mem_req->write(mem_req);
-                wait(cycle);
 
+                wait(cycle);
                 mem_resp = p_mem_resp->read();
                 bvh::Triangle<float> triangle = to_bvh_triangle(mem_resp.trig);
-                bvh::Ray<float> ray = to_bvh_ray(req.ray_and_id.ray);
+                bvh::Ray<float> ray = to_bvh_ray(resp.ist_result.ray_and_id.ray);
                 auto result = triangle.intersect(ray);
                 if (result) {
                     resp.ist_result.ray_and_id.ray.t_max = result->t;
@@ -44,9 +46,9 @@ SC_MODULE(ist_ctrl) {
                     resp.ist_result.u = result->u;
                     resp.ist_result.v = result->v;
                 }
-                wait(cycle);
             }
 
+            wait(cycle);
             p_trv_ctrl_resp->write(resp);
         }
     }
