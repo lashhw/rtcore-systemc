420499c37e071843348a9038cf907d7339449076
Jacky Huang <lashhw81@gmail.com>
2023-06-09 19:48:13 +0800

    lp/hp/ist do not need to process simultaneously

--- src/rtcore/ist.hpp
+++ src/rtcore/ist.hpp
@@ -2,8 +2,8 @@
 #define RTCORE_SYSTEMC_IST_HPP
 
 SC_MODULE(ist) {
-    sync_fifo_in<ist_req_t, num_ist> p_ist_ctrl_in;
-    sync_fifo_out<ist_ctrl_req_t, num_ist> p_ist_ctrl_out;
+    sync_fifo_in<ist_req_t> p_ist_ctrl_in;
+    sync_fifo_out<ist_ctrl_req_t> p_ist_ctrl_out;
     int num_processed = 0;
 
     SC_CTOR(ist) {
@@ -15,32 +15,50 @@ SC_MODULE(ist) {
     }
 
     void thread_1() {
+        std::vector<std::pair<ist_ctrl_req_t, int>> processing;
+
         while (true) {
-            ist_req_t ist_req[num_ist];
-            p_ist_ctrl_in->read(ist_req);
-            delay(ist_latency);
-            ist_ctrl_req_t ist_ctrl_req[num_ist];
-            for (int i = 0; i < num_ist; i++) {
-                ist_ctrl_req[i] = {
-                    .ray_and_id = ist_req[i].ray_and_id,
-                    .num_trigs = ist_req[i].num_trigs - 1,
-                    .trig_addr = ist_req[i].trig_addr + 36,
-                    .intersected = ist_req[i].intersected,
-                    .u = ist_req[i].u,
-                    .v = ist_req[i].v
+            // read: read request
+            advance_to_read();
+            if (p_ist_ctrl_in->readable()) {
+                ist_req_t ist_req = p_ist_ctrl_in->read();
+                ist_ctrl_req_t ist_ctrl_req = {
+                    .ray_and_id = ist_req.ray_and_id,
+                    .num_trigs = ist_req.num_trigs - 1,
+                    .trig_addr = ist_req.trig_addr + 36,
+                    .intersected = ist_req.intersected,
+                    .u = ist_req.u,
+                    .v = ist_req.v
                 };
-                bvh::Triangle<float> triangle = to_bvh_triangle(ist_req[i].trig);
-                bvh::Ray<float> ray = to_bvh_ray(ist_req[i].ray_and_id.ray);
+                bvh::Triangle<float> triangle = to_bvh_triangle(ist_req.trig);
+                bvh::Ray<float> ray = to_bvh_ray(ist_req.ray_and_id.ray);
                 auto result = triangle.intersect(ray);
                 if (result) {
-                    ist_ctrl_req[i].ray_and_id.ray.t_max = result->t;
-                    ist_ctrl_req[i].intersected = true;
-                    ist_ctrl_req[i].u = result->u;
-                    ist_ctrl_req[i].v = result->v;
+                    ist_ctrl_req.ray_and_id.ray.t_max = result->t;
+                    ist_ctrl_req.intersected = true;
+                    ist_ctrl_req.u = result->u;
+                    ist_ctrl_req.v = result->v;
+                }
+
+                processing.emplace_back(ist_ctrl_req, ist_latency);
+                num_processed++;
+            }
+
+            // update: update counter
+            advance_to_update();
+            for (auto &p : processing)
+                p.second = std::min(p.second, p.second - 1);
+
+            // write: send response
+            delay(1);
+            advance_to_write();
+            for (auto it = processing.begin(); it != processing.end(); it++) {
+                if (it->second <= 0) {
+                    p_ist_ctrl_out->write(it->first);
+                    processing.erase(it);
+                    break;
                 }
             }
-            num_processed += num_ist;
-            p_ist_ctrl_out->write(ist_ctrl_req);
         }
     }
 };
--- src/rtcore/lp.hpp
+++ src/rtcore/lp.hpp
@@ -4,8 +4,8 @@
 #include <bvh/node_intersectors.hpp>
 
 SC_MODULE(lp) {
-    sync_fifo_in<bbox_req_t, num_lp> p_bbox_ctrl;
-    sync_fifo_out<trv_ctrl_req_t, num_lp> p_trv_ctrl;
+    sync_fifo_in<bbox_req_t> p_bbox_ctrl;
+    sync_fifo_out<trv_ctrl_req_t> p_trv_ctrl;
     int num_processed = 0;
 
     SC_CTOR(lp) {
@@ -17,36 +17,54 @@ SC_MODULE(lp) {
     }
 
     void thread_1() {
+        std::vector<std::pair<trv_ctrl_req_t, int>> processing;
+
         while (true) {
-            bbox_req_t req[num_lp];
-            p_bbox_ctrl->read(req);
-            delay(lp_latency);
-            trv_ctrl_req_t trv_ctrl_req[num_lp];
-            for (int i = 0; i < num_lp; i++) {
-                trv_ctrl_req[i] = {
+            // read: read request
+            advance_to_read();
+            if (p_bbox_ctrl->readable()) {
+                bbox_req_t bbox_req = p_bbox_ctrl->read();
+                trv_ctrl_req_t trv_ctrl_req = {
                     .type = trv_ctrl_req_t::BBOX
                 };
-                trv_ctrl_req[i].bbox = {
-                    .ray_and_id = req[i].ray_and_id,
-                    .left_node = req[i].left_node,
-                    .right_node = req[i].right_node
+                trv_ctrl_req.bbox = {
+                    .ray_and_id = bbox_req.ray_and_id,
+                    .left_node = bbox_req.left_node,
+                    .right_node = bbox_req.right_node
                 };
-                bvh::Ray<float> ray = to_bvh_ray(req[i].ray_and_id.ray);
+                bvh::Ray<float> ray = to_bvh_ray(bbox_req.ray_and_id.ray);
                 bvh::MPNodeIntersector<bvh::Bvh<float>, mantissa_width, exponent_width> node_intersector(ray);
                 bvh::Bvh<float>::Node left_node = {};
                 bvh::Bvh<float>::Node right_node = {};
                 for (int j = 0; j < 6; j++) {
-                    left_node.bounds[j] = req[i].left_bbox.bounds[j];
-                    right_node.bounds[j] = req[i].right_bbox.bounds[j];
+                    left_node.bounds[j] = bbox_req.left_bbox.bounds[j];
+                    right_node.bounds[j] = bbox_req.right_bbox.bounds[j];
                 }
                 std::pair<float, float> t_left = node_intersector.intersect(left_node, ray, true);
                 std::pair<float, float> t_right = node_intersector.intersect(right_node, ray, true);
-                trv_ctrl_req[i].bbox.left_hit = t_left.first <= t_left.second;
-                trv_ctrl_req[i].bbox.right_hit = t_right.first <= t_right.second;
-                trv_ctrl_req[i].bbox.left_first = t_left.first <= t_right.first;
+                trv_ctrl_req.bbox.left_hit = t_left.first <= t_left.second;
+                trv_ctrl_req.bbox.right_hit = t_right.first <= t_right.second;
+                trv_ctrl_req.bbox.left_first = t_left.first <= t_right.first;
+
+                processing.emplace_back(trv_ctrl_req, lp_latency);
+                num_processed++;
             }
-            num_processed += num_lp;
-            p_trv_ctrl->write(trv_ctrl_req);
+
+            // update: update counter
+            advance_to_update();
+            for (auto &p : processing)
+                p.second = std::min(p.second, p.second - 1);
+
+            // write: send response
+            delay(1);
+            advance_to_write();
+            for (auto it = processing.begin(); it != processing.end(); it++) {
+                if (it->second <= 0) {
+                    p_trv_ctrl->write(it->first);
+                    processing.erase(it);
+                    break;
+                }
+            }
         }
     }
 };
